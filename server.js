f76e8d01bc8f3194601be50cd80eaee047ceb1b2
Carolyn <yintingchien@gmail.com>
2022-08-02 22:01:38 -0400

    building out initial heroku connection

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express')
-const routes = require('./routes')
-const cors = require('cors')
-const logger = require('morgan')
+// const routes = require('./routes')
+// const cors = require('cors')
+// const logger = require('morgan')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
+//middleware
 app.use(express.static(`${__dirname}/frontend/build`))
-app.use(express.json())
-app.use(cors())
-app.use(logger('dev'))
+// app.use(express.json())
+// app.use(cors())
+// app.use(logger('dev'))
 
-app.use('/', routes)
+// app.use('/', routes)
+
+//testing
+app.get('/', (req, res) => {
+  res.send('connect!!')
+})
+
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/frontend/build/index.html`)
 })
